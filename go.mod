179fa003674ac4a2169389673d8c5f575fce7610
matheodrd <matheodurand14@gmail.com>
2025-04-14 23:50:18 +0200

    refactor: use httphelper imported package for HTTP handler utilities

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module supmap-gis
 go 1.24.2
 
 require github.com/caarlos0/env/v11 v11.3.1
+
+require github.com/matheodrd/httphelper v0.1.0 // indirect
--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -2,34 +2,30 @@ package api
 
 import (
 	"fmt"
+	"github.com/matheodrd/httphelper/handler"
 	"net/http"
 	"supmap-gis/internal/domain/services"
 )
 
-type Response[T any] struct {
-	Data    *T     `json:"data,omitempty"`
-	Message string `json:"message,omitempty"`
-}
-
 func (s *Server) geocodeHandler() http.HandlerFunc {
-	return handler(func(w http.ResponseWriter, r *http.Request) error {
+	return handler.Handler(func(w http.ResponseWriter, r *http.Request) error {
 		address := r.URL.Query().Get("address")
 		if address == "" {
-			return NewErrWithStatus(http.StatusBadRequest, fmt.Errorf("missing 'address' query parameter"))
+			return handler.NewErrWithStatus(http.StatusBadRequest, fmt.Errorf("missing 'address' query parameter"))
 		}
 
 		result, err := s.geocodingService.Search(r.Context(), address)
 		if err != nil {
-			return NewErrWithStatus(http.StatusInternalServerError, fmt.Errorf("geocoding address: %w", err))
+			return handler.NewErrWithStatus(http.StatusInternalServerError, fmt.Errorf("geocoding address: %w", err))
 		}
 
-		resp := Response[[]services.Place]{
+		resp := handler.Response[[]services.Place]{
 			Data:    &result,
 			Message: "success",
 		}
 
-		if err := encode[Response[[]services.Place]](resp, http.StatusOK, w); err != nil {
-			return NewErrWithStatus(http.StatusInternalServerError, err)
+		if err := handler.Encode[handler.Response[[]services.Place]](resp, http.StatusOK, w); err != nil {
+			return handler.NewErrWithStatus(http.StatusInternalServerError, err)
 		}
 
 		return nil

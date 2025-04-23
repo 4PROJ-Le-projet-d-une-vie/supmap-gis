824285f0e3cac50a84aba5a0833ce79c342f3377
matheodrd <matheodurand14@gmail.com>
2025-04-24 01:07:27 +0200

    feat: add routeHandler - valhalla routing

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -5,6 +5,7 @@ import (
 	"github.com/matheodrd/httphelper/handler"
 	"net/http"
 	"supmap-gis/internal/domain/services"
+	"supmap-gis/internal/providers/valhalla"
 )
 
 func (s *Server) geocodeHandler() http.HandlerFunc {
@@ -25,6 +26,31 @@ func (s *Server) geocodeHandler() http.HandlerFunc {
 		}
 
 		if err := handler.Encode[handler.Response[[]services.Place]](resp, http.StatusOK, w); err != nil {
+			return handler.NewErrWithStatus(http.StatusInternalServerError, err)
+		}
+
+		return nil
+	})
+}
+
+func (s *Server) routeHandler() http.HandlerFunc {
+	return handler.Handler(func(w http.ResponseWriter, r *http.Request) error {
+		req, err := handler.Decode[valhalla.RouteRequest](r)
+		if err != nil {
+			return handler.NewErrWithStatus(http.StatusBadRequest, err)
+		}
+
+		route, err := s.routingService.CalculateRoute(r.Context(), req)
+		if err != nil {
+			return handler.NewErrWithStatus(http.StatusInternalServerError, err)
+		}
+
+		resp := handler.Response[services.Route]{
+			Data:    route,
+			Message: "success",
+		}
+
+		if err := handler.Encode[handler.Response[services.Route]](resp, http.StatusOK, w); err != nil {
 			return handler.NewErrWithStatus(http.StatusInternalServerError, err)
 		}
 
